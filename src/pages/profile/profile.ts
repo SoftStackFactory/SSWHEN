cf2be2691d8e6b259af71e1ec5886a4cba640936
James Orduno <jorduno19@gmail.com>
2018-01-22 19:36:16 +0000

    75%

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ProfilePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -14,8 +8,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'profile.html',
 })
 export class ProfilePage {
-
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.results = "graph";
   }
 
   ionViewDidLoad() {
