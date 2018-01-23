eb446383899ecdc1eab7ce564df1a32f2c38535d
James Orduno <jorduno19@gmail.com>
2018-01-23 02:41:17 +0000

    fixed mock data

--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -10,22 +10,23 @@ import { RegisterPage } from '../register/register';
 })
 export class ResultsPage {
   
-  
+  data: any = [
+    { age: 62, monthlyPayout: 1000 },
+    { age: 63, monthlyPayout: 1000 },
+    { age: 64, monthlyPayout: 1000 },
+    { age: 65, monthlyPayout: 1000 },
+    { age: 66, monthlyPayout: 1000 },
+    { age: 67, monthlyPayout: 1000 },
+    { age: 68, monthlyPayout: 1000 },
+    { age: 69, monthlyPayout: 1000 },
+    { age: 70, monthlyPayout: 1000 }
+    ];
+  results: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    
     this.results = "graph";
     
-  //   var results[] = [
-  //   { age: 62, monthlyPayout: 1000 },
-  //   { age: 63, monthlyPayout: 1000 },
-  //   { age: 64, monthlyPayout: 1000 },
-  //   { age: 65, monthlyPayout: 1000 },
-  //   { age: 66, monthlyPayout: 1000 },
-  //   { age: 67, monthlyPayout: 1000 },
-  //   { age: 68, monthlyPayout: 1000 },
-  //   { age: 69, monthlyPayout: 1000 },
-  //   { age: 70, monthlyPayout: 1000 }
-  //   ];
   }
   goToRegister(params){
     if (!params) params = {};
