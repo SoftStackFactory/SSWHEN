57f67ad81816450e9b260bf2e35e05c9e7736e2d
Peter Horton <phorton@softstackfactory.org>
2018-01-23 06:04:48 +0000

    Updated styling

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from "@angular/forms";
 import { DashboardPage } from '../dashboard/dashboard';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, ModalController } from 'ionic-angular';
 
 /**
  * Generated class for the ProfilePage page.
@@ -17,10 +17,10 @@ import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angu
 })
 export class ProfilePage {
 
-
   constructor(public navCtrl: NavController, 
   public navParams: NavParams,
-  private alertCtrl: AlertController
+  private alertCtrl: AlertController,
+  public modalCtrl: ModalController
   ) {
     
    
@@ -38,5 +38,16 @@ export class ProfilePage {
 popView(){
     this.navCtrl.push(DashboardPage);
     }
+    
+//   ValidateEmail(mail) 
+// {
+// if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
+//   {
+//     return (true)
+//   }
+//     alert("You have entered an invalid email address!")
+//     return (false)
+//     console.log("User entered an invalid email address")
+// }
 
 }
