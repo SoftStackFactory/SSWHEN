37fa98b5488888c3ca6175617277f1c67539cbd4
Andy Hester <ahester34@gmail.com>
2018-01-27 23:29:43 +0000

    first data table commit

---
+++ src/pages/data-table/data-table.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the DataTablePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-data-table',
+  templateUrl: 'data-table.html',
+})
+export class DataTablePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+  
+  //mock data array for testing
+  
+  benefitData: any[] = [
+      {
+        age: 62,
+        monthly: 1000,
+        cumulative: 10000
+      },
+      {
+        age:63, 
+        monthly: 2000,
+        cumulative: 20000
+      },
+      {
+        age: 64,
+        monthly: 3000,
+        cumulative: 30000
+      }
+    ]
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad DataTablePage');
+  }
+
+}
