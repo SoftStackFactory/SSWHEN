26d15fbdcfc87bc2c72bb61cb399a1c20aa7dee0
Andy Hester <ahester34@gmail.com>
2018-02-01 05:21:42 +0000

    YYYY-MM converts to number of months

--- src/pages/info-input/info-input.ts
+++ src/pages/info-input/info-input.ts
@@ -15,23 +15,39 @@ export class InfoInputPage {
   myDate: string;
   gender: string;
   monthlyAtFRA: number;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, public bene$: BenefitProvider) {
-    this.bene$.dob = this.myDate;
-    this.bene$.gender = this.gender;
-    this.bene$.pia = this.monthlyAtFRA;
+  dateInMonths: number;
+  
+  convertToMonths() {
+    let dateString: string = this.myDate;
+    let yearString: any = dateString.substr(0,4);
+    let monthString: any = dateString.substr(5,6);
+    this.dateInMonths = (Number(yearString)*12) + Number(monthString);
+    console.log(this.dateInMonths, "hit");
+  }
+  
+  
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams, 
+    public bene$: BenefitProvider) {
+      
+      this.bene$.dob = this.dateInMonths;
+      this.bene$.gender = this.gender;
+      this.bene$.pia = this.monthlyAtFRA;
   }
 
   logForm() {
-      console.log(this.myDate);
-      console.log(this.gender);
-      console.log(this.monthlyAtFRA);
-      console.log(this.bene$.monthlyBenefit(this.monthlyAtFRA));
+    this.convertToMonths();
+      // console.log(this.myDate);
+      // console.log(this.gender);
+      // console.log(this.monthlyAtFRA);
+      // console.log(this.dateInMonths);
       // this.navCtrl.push(ResultsPage, {
       //   data: this.monthlyAtFRA
       
       // })
   }
+  
+
 
 }
 
