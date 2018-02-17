8356e123f60f84b1d21ef71f6a9435106bd71740
Sayed <awsayed@alaska.edu>
2018-02-17 20:49:58 +0000

    Dashboard invokes mockDataProvider when calculation provider is not available

--- src/providers/mock-data/mock-data.ts
+++ src/providers/mock-data/mock-data.ts
@@ -6,19 +6,26 @@ import { map } from 'rxjs/operator/map';
 // The service could just as well get data from local storage or from the back end
 import {SSUser} from '../../models/SSUser';
 import { mockUsers } from '../../models/mockData/mockUsers';
-
+import {Results} from '../../models/Results';
+import { MOCKRESULTS } from '../../models/mockData/mockResults';
 
 export class MockDataProvider {
 
   constructor() {}
 
-  // getData(): SSUser[] {
-  // return mockUsers;
-  // }
+  getUsers(): SSUser[] {
+    return mockUsers;
+  }
+  
+  getResults(): Results[] {
+    return MOCKRESULTS;
+  }
+  
+  
   
 // method to return an Observable. This is needed when making requests to a remote server
-  getData(): Observable<SSUser[]> {
-    return of(mockUsers);
-  }
+  // getData(): Observable<SSUser[]> {
+  //   return of(mockUsers);
+  // }
 
 }
