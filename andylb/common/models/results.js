aa6e086afd0e65d8dce8ded8666c965ec25defb1
Andy Hester <ahester34@gmail.com>
2018-02-19 03:40:33 +0000

    created http interaction for custom api, commented out logic

--- andylb/common/models/results.js
+++ andylb/common/models/results.js
@@ -1,5 +1,33 @@
 'use strict';
 
 module.exports = function(Results) {
+    Results.remoteMethod("benefitData", {
+        http: {
+            path: "/benefitData",
+            verb: "GET"
+        },
+        accepts: [{
+            arg: "pia",
+            required: true,
+            type: "number",
+            description: "SS Benefit at full retirement age"
+    }, {
+            arg: "gender",
+            required: true,
+            type: "string",
+            description: "m or f"
+    }, {
+            arg: "dob",
+            required: true,
+            type: "string",
+            description: "Month and Year of birth (MM-YYYY)"
+    }],
+        notes: ["This api will return estimates for Social Security benefits over a range of ages."],
+        description: "Calculates benefit estimates.",
+        returns: {
+            type: 'object',
+            root: true
+        }
+    });
 
 };
