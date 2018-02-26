d1fbd1421284cf83a8bfd77e888c8bede2479f8f
dcarandangssf <dcarandang@softstackfactory.org>
2018-02-26 06:33:29 +0000

    added getUser method

--- src/providers/ss-users/ss-users.ts
+++ src/providers/ss-users/ss-users.ts
@@ -18,27 +18,33 @@ export class SsUsersProvider {
 
   login(user) {
     let path = '/SSUsers/login';
-    return this.http.post(this.apiUrl + path, user).map(res => res.json())
+    return this.http.post(this.apiUrl + path, user).map( res => res.json())
   }
   
   register(user) {
     let path = '/SSUsers';
-    return this.http.post(this.apiUrl + path, user).map(res => res.json())
+    return this.http.post(this.apiUrl + path, user).map( res => res.json())
+    
   }
   
   updateUser(user, token) {
     let path = '/SSUsers/' + user.id + '/replace?access_token=' + token;
-    return this.http.post(this.apiUrl + path, user).map(res => res.json())
+    return this.http.post(this.apiUrl + path, user).map( res => res.json())
   }
 
   logout(token) {
     let path = '/SSUsers/?access_token=' + token;
-    return this.http.post(this.apiUrl + path, {}).map(res => res.json())
+    return this.http.post(this.apiUrl + path, {}).map( res => res.json())
   }
   
   getResultsByUser(user, token) {
     let path = '/SSUsers/' + user.id + '/results?access_token=' + token;
-    return this.http.post(this.apiUrl + path, user).map(res => res.json())
+    return this.http.post(this.apiUrl + path, user).map( res => res.json())
+  }
+  
+  getUser(userId, token) {
+    let path = '/SSUsers/' + userId + '?access_token=' + token;
+    return this.http.get(this.apiUrl + path).map( res => res.json())
   }
 
 }
