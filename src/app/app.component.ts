1631b0731729448dc8216668c564279449fe451c
Khoa Nguen <visthetic@gmail.com>
2018-03-05 11:26:38 -0800

    Reverting home app debug changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import {HomePage} from '../pages/home/home';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = LandingPage;
   test: string = "test";
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
