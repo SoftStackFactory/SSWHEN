8aa2f686add870b342eb0ac4c6611d3a5741280e
Sayed <awsayed@alaska.edu>
2018-04-15 00:40:43 +0000

    Resolved an issue with SsUsersProvider.updateUser not able to be called

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { DashboardPage } from '../dashboard/dashboard';
-import {IonicPage, NavController, NavParams, AlertController, ViewController} from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, ViewController } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SsUsersProvider } from '../../providers/ss-users/ss-users';
 import { Storage } from '@ionic/storage';
@@ -22,9 +22,11 @@ export class LoginPage {
   myForm: FormGroup;
   submitAttempt: boolean = false;
   ssUser: SSUser;
+  errorMessage: any;
+  isError: boolean = false;
 
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
     public formBuilder: FormBuilder,
     public alertCtrl: AlertController,
@@ -33,65 +35,83 @@ export class LoginPage {
     public viewCtrl: ViewController
   ) {
     this.myForm = formBuilder.group({
-      email: ['', 
+      email: ['',
         Validators.compose([
-            Validators.required,
-            Validators.pattern('^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$'),
-            Validators.maxLength(30),
-          ])
+          Validators.required,
+          Validators.pattern('^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$'),
+          Validators.maxLength(30),
+        ])
       ],
-      password: ['', 
+      password: ['',
         Validators.compose([
-            Validators.required,
-            Validators.pattern(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){6,12}$/)
-          ])
+          Validators.required,
+          Validators.pattern(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){6,12}$/)
+        ])
       ]
     });
-    console.log("What the form captures:",this.myForm);
+    console.log("What the form captures:", this.myForm);
   }
 
   ionViewDidLoad() {
     this.viewCtrl.setBackButtonText('Back');
   }
 
-  popView(){
-      this.submitAttempt = true;
-      if(!this.myForm.valid) {
-        console.log("Unsuccessful login :(", this.myForm); 
-      } else {
-        // this.myForm.value is an {} containing properties email and password
-        this.ssUsersProvider.login(this.myForm.value).subscribe( res => {
-          console.log("The return of the ssUsersProvider.login http.post request is: ",res);
-          // res is an {} with the properties created, id, ttl, userId
-          // We are interested in id (token), and userId
-          this.storage.set('userId', res.userId);
-          this.storage.set('token', res.id);
-            
-            this.ssUsersProvider.getUser(res.userId, res.id)
-              .subscribe( res => {
-                let alert = this.alertCtrl.create({
-                title: '',
-                subTitle: 'You are logged in',
-                buttons: ['OK']
-                });
-                alert.present();
-                // alert('Thank you for loging in!');
-                console.log("Successful login", this.myForm.value);
-                this.ssUser = res;
-                console.log('Response from ssUsersProvider.getUser',this.ssUser);
-                this.storage.set('SSUser', res);
-                this.navCtrl.push(DashboardPage);
-                }, err => {
-                  console.log(err)
-                });
+  popView() {
+    this.submitAttempt = true;
+    if (!this.myForm.valid) {
+      console.log("Unsuccessful login :(", this.myForm);
+    } else {
+      // this.myForm.value is an {} containing properties email and password
+      console.log("Successful login", this.myForm.value);
+      this.ssUsersProvider.login(this.myForm.value).subscribe(res => {
+        console.log("The return of the ssUsersProvider.login http.post request is: ", res);
+        // res is an {} with the properties created, id, ttl, userId
+        // We are interested in id (token), and userId
+        this.storage.set('userId', res.userId);
+        this.storage.set('token', res.id);
+
+        this.ssUsersProvider.getUser(res.userId, res.id).subscribe(res => {
+            let alert = this.alertCtrl.create({
+              title: '',
+              subTitle: 'You are logged in',
+              buttons: ['OK']
+            });
+            alert.present();
+            // ssUsersProvider.getUser res is an SSUser except with id instead of password
+            // Due to the way the back end code written in sswhen-bk/common/models/ss-user.json, password is a required field in the SSUser model
+            // Therefore the user's password has to be added to the SSUser model thats passed into ssUsersProvider's methods
+            // This is a security risk best avoided if the backend code makes the password optional instead of required
+            this.ssUser = res;
+            this.ssUser.password = this.myForm.value.password;
+            console.log('Response from ssUsersProvider.getUser', this.ssUser);
+            this.storage.set('SSUser', res);
+            this.navCtrl.push(DashboardPage);
           }, err => {
-            // handle common error codes, 401, 422, 500, etc.
-            console.log(err);
+            console.log(err)
           });
-      }
+      }, err => {
+        this.isError = true;
+        console.log('error message: ', err);
+        // handle common error codes, 401, 422, 500, etc.
+        if (err.status === 0) {
+          this.errorMessage = 'Server Timeout';
+        }
+        else if (err.status === 401) {
+          this.errorMessage = 'Unauthorized Access, check credentials';
+        } else if (err.status === 404) {
+          this.errorMessage = 'User was not found, check email';
+        } else if (err.status === 422) {
+          this.errorMessage = 'Unprocessable Entity';
+        } else if (err.status === 500) {
+          this.errorMessage = 'Server is offline';
+        } else {
+          this.errorMessage = 'Unable to process request' + err.status + ': ' + err.statusText;
+        }
+      });
     }
-  
-    sendLink() {
+  }
+
+  sendLink() {
     let prompt = this.alertCtrl.create({
       title: 'Forgot Password?',
       message: "Type in your email address and a link will be sent to your inbox to reset your password:",
@@ -118,15 +138,15 @@ export class LoginPage {
       ]
     });
     prompt.present();
-    }
-    
-    anotherAlert() {
-      let alert = this.alertCtrl.create({
-        title: '',
-        subTitle: 'An email has been sent to your address with a reset link. Please follow the link to set a new password',
-        buttons: ['OK']
-      });
-      alert.present();
-    }
-  
+  }
+
+  anotherAlert() {
+    let alert = this.alertCtrl.create({
+      title: '',
+      subTitle: 'An email has been sent to your address with a reset link. Please follow the link to set a new password',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
 }
